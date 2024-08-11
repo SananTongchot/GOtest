a2c21cacfc52a236e402b8618dd8b91aa7ae2685
SananTongchot <161601715+SananTongchot@users.noreply.github.com>
2024-08-11 14:10:08 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world Lotto888")
+}
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprint(w, "Hello world Lotto888")
+// 	})
+// 	fmt.Println("Server on port 5000")
+// 	http.ListenAndServe(":5000", nil)
+// }
